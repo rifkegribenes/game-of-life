fdeab93150eccd5921f4b8576b726345da79bbb7
Sarah Schneider <rifkegribenes@gmail.com>
2017-06-30 22:25:58 -0700

    added pre-commit hook

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import Home from './containers/Home';
+import About from './containers/About';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <main>
+          <h1>hello world!</h1>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </main>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
