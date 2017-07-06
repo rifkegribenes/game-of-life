aebcfd6663b2baf154f662f6e5dfa19bf092cb2a
Sarah Schneider <rifkegribenes@gmail.com>
2017-07-06 16:38:46 -0700

    initial notes on app structure

--- src/containers/Cell.jsx
+++ src/containers/Cell.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const GameBoard = () => (
+const Cell = () => (
   <div>
     <h2>GameBoard</h2>
   </div>
 );
 
-export default GameBoard;
+export default Cell;
---
+++ src/containers/Controls.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Controls = () => (
+  <div>
+    <h2>GameBoard</h2>
+  </div>
+);
+
+export default Controls;
